7f36e4a3479381bf81bf37272e39706ea5bdc8ba
Vitaliy <svetlov.vu@gmail.com>
2019-05-03 21:51:24 +0400

    NotWork

--- request.js
+++ request.js
@@ -20,17 +20,6 @@ function requestToServer() {
       let newDiv = document.createElement('div');
         newDiv.className = 'dataNameUser';
         newDiv.innerHTML = `<a class='link' href='#user${oneUser.name.first}' onclick='fon.style.display="block"'><table><tr><td class="firstColl"><img class='avatarUserSmall' src="${oneUser.picture.thumbnail}" alt="smallAvatar" /></td><td class="tooColl"><span class='nameUser'><span>${oneUser.name.title}</span><span class='nameUserFirst'>${oneUser.name.first}</span><span class='nameUserUpper'>${oneUser.name.last}</span></span></td></tr></table></a>
-                            <div class='fullDataNameUser' id='user${oneUser.name.first}'>
-                              <table>
-                                <tr><td class='firstCollFullData' rowspan="6"><img class='avatarUserBig' src="${oneUser.picture.large}" alt="bigAvatar" /></td><td><div>Имя</div></td><td><div class='nameUserUpper'>${oneUser.name.first}</div></td></tr>
-                                <tr><td><div>Фамилия</div></td><td><div class='nameUserUpper'>${oneUser.name.last}</div></td></tr>
-                                <tr><td><div>Улица</div></td><td><div class='nameUpper'>${oneUser.location.street}</div></td></tr>
-                                <tr><td><div>Город</div></td><td><div class='nameUpper'>${oneUser.location.city}</div></td></tr>
-                                <tr><td><div>Штат</div></td><td><div class='nameUpper'>${oneUser.location.state}</div></td></tr>
-                                <tr><td><div>Телефон</div></td><td><div>${oneUser.phone}</div></td></tr>
-                              </table>
-                              <a href='#close' class="close" title='Закрыть' onclick='fon.style.display="none"'>&times</span>
-                              </div>`;
       demo.appendChild(newDiv);
     }
   }
